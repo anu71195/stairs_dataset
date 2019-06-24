5c9f837c4aaa5491a416d5d43b017a3ad931feb1
Anurag <anu71195@gmail.com>
2019-06-24 23:49:50 +0530

    code for data augmentation done

--- data_augmentation.py
+++ data_augmentation.py
@@ -8,7 +8,19 @@ import numpy as np
 import cv2
 from PIL import Image
 import os
+import shutil
 from os import walk
+
+
+def check_create_directory(directories,new_dataset_preposition):
+	for directory_loc in directories:
+		directory_loc=directory_loc.split("/")
+		directory=new_dataset_preposition
+		for loc_part in directory_loc:
+			directory=directory +(loc_part+"/")
+			print(directory)
+			if not os.path.exists(directory):
+				os.makedirs(directory)
 
 def get_file_list(location):
 	f = []
@@ -23,18 +35,38 @@ def get_dataset(locations):
 		file_list.append(get_file_list(i))
 	return file_list
 
-def give_path_to_images(file_list,locations):
+def give_path_to_images(file_list,locations,new_dataset_preposition,clear_augment_dataset):
+	if clear_augment_dataset:
+		try:
+			shutil.rmtree(new_dataset_preposition+"dataset")
+
+		except:
+			pass
+	# exit()
+	check_create_directory(locations,new_dataset_preposition)
 	for i in range(len(file_list)):
 		for j in range(len(file_list[i])):
 			file_list[i][j]=locations[i]+"/"+file_list[i][j]
 	return file_list
 
 
-def augment_data(file_location):
+def augment_data(file_location,new_dataset_preposition):
+
+
 	for i in file_location:
 		for j in i:
+			print(j)
 			image=cv2.imread(j)
-			cv2.imwrite("aug_"+j+"_noise",noisy(image))
+			new_j=new_dataset_preposition+j.split(".")[0]+"_noise.jpg"
+			print(new_j)
+			cv2.imwrite(new_j,noisy(image))
+			new_j=new_dataset_preposition+j.split(".")[0]+"_flip.jpg"
+			print(new_j)
+			cv2.imwrite(new_j,horizontal_flip(image))
+			new_j=new_dataset_preposition+j.split(".")[0]+"_rotate.jpg"
+			print(new_j)
+			cv2.imwrite(new_j,rotation(image))
+
 
 
 def rotation(img):
@@ -53,16 +85,38 @@ def horizontal_flip(image_array: ndarray):
     # horizontal flip doesn't need skimage, it's easy as flipping the image array of pixels !
     return image_array[:, ::-1]
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#it is expected that no changes in the directory names are made in the dataset including the directory dataset
+
+
+
+
+new_dataset_preposition="aug_"
+clear_augment_dataset=1;
+
 locations=[]
 locations.append("dataset/no_stairs")
 locations.append("dataset/stairs/down")
 locations.append("dataset/stairs/up")
 
+print("gathering image list...\n")
 file_list=get_dataset(locations)
-file_location=give_path_to_images(file_list,locations)
-augment_data(file_location)
+print("gathering image locations...\n")
+file_location=give_path_to_images(file_list,locations,new_dataset_preposition,clear_augment_dataset)
+print("augmenting data....\n")
+augment_data(file_location,new_dataset_preposition)
 
-# image=cv2.imread("IMG_20190510_181157.jpg");
-# cv2.imwrite("img_noise.jpg",noisy(image))
-# cv2.imwrite("img_flip.jpg",horizontal_flip(image))
-# cv2.imwrite("img_rot.jpg",rotation(image))
