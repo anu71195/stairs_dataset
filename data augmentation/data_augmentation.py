7bef60954adb3197bff228f82ee31d88cc0aea71
Anurag <anu71195@gmail.com>
2019-06-26 00:58:43 +0530

    images hsown in imshow and after saving seems different

--- data augmentation/data_augmentation.py
+++ data augmentation/data_augmentation.py
@@ -17,7 +17,11 @@ def rotation(img):
 	dst = cv2.warpAffine(img,M,(cols,rows))
 	return dst
 
-def noisy(image):  
+def horizontal_flip(image_array: ndarray):
+    # horizontal flip doesn't need skimage, it's easy as flipping the image array of pixels !
+    return image_array[:, ::-1]
+
+def gaussian_noisy(image):  
   row,col,ch= image.shape
   mean = 20
   sigma =40**0.5
@@ -26,31 +30,96 @@ def noisy(image):
   print(noise)
   print(image+noise)
   return (image + noise)
+
+def salt_pepper(image):
+  row,col,ch = image.shape
+  s_vs_p = 0.5
+  amount = 0.1
+  out = np.copy(image)
+  # Salt mode
+  num_salt = np.ceil(amount * image.size * s_vs_p)
+  coords = [np.random.randint(0, i - 1, int(num_salt))
+          for i in image.shape]
+  out[coords] = 1
+
+  # Pepper mode
+  num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+  coords = [np.random.randint(0, i - 1, int(num_pepper))
+          for i in image.shape]
+  out[coords] = 0
+  return out
+
+def poisson(image):
+    vals = len(np.unique(image))
+    vals = 2 ** np.ceil(np.log2(vals))
+    noisy = np.random.poisson(image * vals) / float(vals)
+    return noisy  
+
+def speckle(image):
+  row,col,ch = image.shape
+  gauss = np.random.randn(row,col,ch)
+  gauss = gauss.reshape(row,col,ch)
+  print(gauss)        
+  noisy = image + image * gauss
+  print(image)
+  print(noisy)
+  return noisy
    
-def horizontal_flip(image_array: ndarray):
-    # horizontal flip doesn't need skimage, it's easy as flipping the image array of pixels !
-    return image_array[:, ::-1]
 
 
-image=cv2.imread("IMG_20190510_181157.jpg");
-noisy_image=noisy(image)
-print(noisy_image)
+def plotnoise(img, modei, r, c, i):
+    plt.subplot(r,c,i)
+    if (modei is not None):
+        gimg = util.random_noise(img, mode=modei)
+        filename="image"+str(modei)+".jpg"
+        print(filename)
+        cv2.imwrite(filename,gimg*255)
+        plt.imshow(gimg)
+    else:
+        plt.imshow(img)
+    plt.title(modei)
+    plt.axis("off")
 
 
-fig=plt.figure(figsize=(1,2))
-fig.add_subplot(1,1,1)
-plt.imshow(image)
 
 
-cv2.imwrite("img_noise.jpg",noisy_image)
-image=cv2.imread("img_noise.jpg")
-print(image)
-fig.add_subplot(1,2,2)
-plt.imshow(image)
+image=cv2.imread("IMG_20190513_173038_0_rotate.jpg");
+
+
+img=image
+
+plt.figure(figsize=(18,24))
+r=4
+c=2
+plotnoise(img, "gaussian", r,c,1)
+plotnoise(img, "localvar", r,c,2)
+plotnoise(img, "poisson", r,c,3)
+plotnoise(img, "salt", r,c,4)
+plotnoise(img, "pepper", r,c,5)
+plotnoise(img, "s&p", r,c,6)
+plotnoise(img, "speckle", r,c,7)
+plotnoise(img, None, r,c,8)
 plt.show()
-# cv2.imwrite("img_noise.jpg",noisy(image))
-# cv2.imwrite("img_flip.jpg",horizontal_flip(image))
+
+
+# noisy_image=speckle(image)
+# # print(noisy_image)
+
+
+# fig=plt.figure(figsize=(1,2))
+# fig.add_subplot(1,1,1)
+# plt.imshow(image)
+
+
+# cv2.imwrite("img_noise.jpg",noisy_image)
+# image=cv2.imread("img_noise.jpg")
+# # print(image)
+# fig.add_subplot(1,2,2)
+# plt.imshow(noisy_image)
+# plt.show()
+# # cv2.imwrite("img_noise.jpg",noisy(image))
+# # cv2.imwrite("img_flip.jpg",horizontal_flip(image))
 
 
 
-cv2.waitKey(0)
+# cv2.waitKey(0)
--- data_augmentation.py
+++ data_augmentation.py
@@ -51,7 +51,7 @@ def give_path_to_images(file_list,locations,new_dataset_preposition,clear_augmen
 	return file_list
 
 
-def augment_data(file_location,new_dataset_preposition,min_degree,max_degree,mean_array,std_array,noises):
+def augment_data_1(file_location,new_dataset_preposition,min_degree,max_degree,mean_array,std_array,noises):
 	max_degree+=1;#in python the maxima is open bound not close bound so adding 1 to max degree
 	for i in file_location:
 		for j in i:
@@ -70,11 +70,12 @@ def augment_data(file_location,new_dataset_preposition,min_degree,max_degree,mea
 				cv2.imwrite(new_j,rotation(flip_image,degrees))
 
 			for noise in noises:
+				print(noise)
 				if(noise=="gaussian"):	
 					for mean in mean_array:
 						for std in std_array:
 
-							noisy_image=noisy(image,mean,std)
+							noisy_image=gaussian(image,mean,std)
 							for degrees in range(min_degree,max_degree):
 								new_j=new_dataset_preposition+j.split(".")[0]+"_"+str(mean)+"_"+str(std)+"_"+noise+"_noise_"+str(degrees)+"_rotate.jpg"
 								print(new_j)
@@ -87,15 +88,24 @@ def augment_data(file_location,new_dataset_preposition,min_degree,max_degree,mea
 								print(new_j)
 								cv2.imwrite(new_j,rotation(flip_image,degrees))
 
-				elif(noise=="salt&pepper"):
-					pass;
+				elif(noise=="s&p"):					
+					noisy_image=salt_pepper(image)
+					for degrees in range(min_degree,max_degree):
+						new_j=new_dataset_preposition+j.split(".")[0]+"_"+noise+"_noise_"+str(degrees)+"_rotate.jpg"
+						print(new_j)
+						cv2.imwrite(new_j,rotation(noisy_image,degrees))
+
+
+					flip_image=horizontal_flip(noisy_image)
+					for degrees in range(min_degree,max_degree):
+						new_j=new_dataset_preposition+j.split(".")[0]+"_"+noise+"_noise_flip_"+str(degrees)+"_rotate.jpg"
+						print(new_j)
+						cv2.imwrite(new_j,rotation(flip_image,degrees))
+					  
 				elif(noise=="poisson"):
 					pass;
 				elif(noise=="speckle"):
 					pass;
-
-			exit()
-
 
 
 def rotation(img,degrees):
@@ -104,14 +114,83 @@ def rotation(img,degrees):
 	dst = cv2.warpAffine(img,M,(cols,rows))
 	return dst
 
-def noisy(image,mean,var):  
+def gaussian(image,mean,var):  
   row,col,ch= image.shape
   print(mean,var)
   return (image + np.random.normal(mean,var,image.shape))
+
+def salt_pepper(image):
+  row,col,ch = image.shape
+  s_vs_p = 0.5
+  amount = 0.1
+  # Salt mode
+  num_salt = np.ceil(amount * image.size * s_vs_p)
+  coords = [np.random.randint(0, i - 1, int(num_salt))
+          for i in image.shape]
+  image[coords] = 1
+
+  # Pepper mode
+  num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+  coords = [np.random.randint(0, i - 1, int(num_pepper))
+          for i in image.shape]
+  image[coords] = 0
+  return image
    
 def horizontal_flip(image_array: ndarray):
     # horizontal flip doesn't need skimage, it's easy as flipping the image array of pixels !
     return image_array[:, ::-1]
+
+
+
+
+def augment_data(file_location,new_dataset_preposition,min_degree,max_degree,mean_array,std_array,noises):
+	max_degree+=1;#in python the maxima is open bound not close bound so adding 1 to max degree
+	fig=plt.figure(figsize=(18,24))
+	counter=1
+	for i in file_location:
+		for j in i:
+			print(j)
+			image=(cv2.imread(j))/255.0
+
+			for degrees in range(min_degree,max_degree):
+				new_j=new_dataset_preposition+j.split(".")[0]+"_"+str(degrees)+"_rotate.jpg"
+				print(new_j)
+				rotated_image=rotation(image,degrees)
+				cv2.imwrite(new_j,rotated_image*255.0)
+				plt.subplot(4,2,counter)
+				plt.imshow(rotated_image)
+				counter+=1
+
+			flip_image=horizontal_flip(image)
+			for degrees in range(min_degree,max_degree):
+				new_j=new_dataset_preposition+j.split(".")[0]+"_flip_"+str(degrees)+"_rotate.jpg"
+				print(new_j)
+				rotated_image=rotation(flip_image,degrees)
+				cv2.imwrite(new_j,rotated_image*255.0)
+
+
+			for noise in noises:
+				noisy_image = util.random_noise(image, mode=noise)
+				for degrees in range(min_degree,max_degree):
+					new_j=new_dataset_preposition+j.split(".")[0]+"_"+noise+"_noise_"+str(degrees)+"_rotate.jpg"
+					print(new_j)
+					rotated_image=rotation(noisy_image,degrees)
+					cv2.imwrite(new_j,rotated_image*255.0)
+					plt.subplot(4,2,counter)
+					plt.imshow(rotated_image)
+					counter+=1
+
+
+				flip_image=horizontal_flip(noisy_image)
+				for degrees in range(min_degree,max_degree):
+					new_j=new_dataset_preposition+j.split(".")[0]+"_"+noise+"_noise_flip_"+str(degrees)+"_rotate.jpg"
+					print(new_j)
+					rotated_image=rotation(flip_image,degrees)
+					cv2.imwrite(new_j,rotated_image*255.0)
+
+
+			plt.show()
+			exit()
 
 
 
@@ -134,19 +213,17 @@ def horizontal_flip(image_array: ndarray):
 
 
 new_dataset_preposition="augmented_"
-noises=["gaussian","salt&pepper","poisson","speckle"]
+noises=["gaussian","localvar","s&p","poisson","speckle","salt","pepper"]
 clear_augment_dataset=1;
 min_degree=-0
 max_degree=0
 mean_array=np.arange(-3,4,3)
-# var_array=np.arange(50,400,10)
-# std_array=np.sqrt(var_array)
 std_array=np.arange(80,126,15)
 
 locations=[]
+locations.append("dataset/stairs/up")
 locations.append("dataset/no_stairs")
 locations.append("dataset/stairs/down")
-locations.append("dataset/stairs/up")
 
 print("gathering image list...\n")
 file_list=get_dataset(locations)
